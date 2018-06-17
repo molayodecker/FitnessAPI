ad29f20f4e42cc734afab7ac1141ed68e2f787ef
Arthur Decker <molayodecker@gmail.com>
2018-06-17 00:12:34 -0400

    Moved User and UserList class from main app to resource

---
+++ resources/user.py
@@ -0,0 +1,33 @@
+from flask import jsonify
+from flask_restful import Resource
+from models.user import UserModel
+from models.user_profile import UserProfileModel
+from schemes.user import UserSchema
+from helpers.auth import Tokenizer
+from controllers.auth import Controller
+from webargs.flaskparser import use_args, use_kwargs
+
+
+class User(Resource):
+    @Tokenizer.token_required
+    def get(self, logged_in_user):
+        #users = Controller(UserProfileModel).find_all(self['id'])
+        users = UserProfileModel.query.filter_by(user_id = 3)
+        print(users)
+        user = Controller(UserModel).find_by_public_id(self['public_id'])
+        if not user:
+            return jsonify({ 'message': 'No user found'})
+        user_data = UserSchema().dump(user).data  
+        return [{ **user_data }]
+
+
+class UserList(Resource):
+    @Tokenizer.token_required
+    def get(self, logged_in_user):
+        return jsonify({'message': 'Hello'})
+       
+
+    @use_args(UserSchema(many = True))
+    def post(self, args):
+        database.session.add_all(args)
+        database.session.commit()
