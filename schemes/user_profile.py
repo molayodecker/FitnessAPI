ce8a36500025321e263ccf96100a7bec89fa2741
Arthur Decker <molayodecker@gmail.com>
2018-06-17 15:15:07 -0400

    =Fixed problem committing data to user_profile

--- schemes/user_profile.py
+++ schemes/user_profile.py
@@ -1,7 +1,12 @@
+from flask import request
 from flask_essentials import database
 from marshmallow_sqlalchemy import ModelSchema
 from marshmallow import Schema, fields, pre_load, post_load, pre_dump, post_dump, validates, fields, ValidationError, pprint
 from models.user_profile import UserProfileModel
+from models.user import UserModel
+# from schemes.user import UserSchema
+import jwt
+from controllers.auth import Controller
 
 class UserProfileSchema(ModelSchema):
     class Meta:
@@ -11,8 +16,16 @@ class UserProfileSchema(ModelSchema):
 
     @pre_load(pass_many = True)
     def addprofile(self, args, many):
-        pass
-        # for values in args:
-        #     new_profile = UserProfileModel(age=values['age'],gender=values['gender'],email= values['email'],weight= values['weight'],height=values['height'],goal=values['goal'],calories=values['calories'],loss=values['loss'],gain=values['gain'],user_id= self['id'])
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+            data = jwt.decode(token, 'f7yD5o~5%rw28H.' , algorithms='HS256')
+            user_data = Controller(UserModel).find_by_public_id(data['public_id'])
+        for values in args:
+            new_profile = UserProfileModel(age=values['age'],gender=values['gender'],\
+            email= values['email'],weight= values['weight'],height=values['height'],\
+            goal=values['goal'],calories=values['calories'],loss=values['loss'],\
+            gain=values['gain'],user= user_data)
+            database.session.add(new_profile)
+            database.session.commit()
 
     
